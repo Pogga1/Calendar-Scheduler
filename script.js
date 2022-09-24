ae99e8750a178dc08e32adcd47b9e00bcbeb4dec
Pogga1 <Andreweysoldt@gmail.com>
2022-09-24 15:00:21 -0700

    redid javaScript and made website interactive and functional

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-let currentDay= document.getElementById("currentday")
-let row1= document.getElementsByClassName('row1');
-let saveButton=document.getElementsByClassName('saveBtn')
+let saveButton = $(".saveBtn");
 
+$(document).ready(function() {
+    currentDay = moment().format("MMM/DD/YYYY");
+    $("#currentDay").text(currentDay);
+  
+    function currentTime() {
+      var current = moment().hours();
+      var time = $(".time-block");
+  
+      time.each(function(){
+          var hour= parseInt($(this).attr('id'))
+          if (hour===current){
+              $(this).children('.col-sm-10').attr("class", "present col-sm-10 description")
+          } else if (current > hour){
+            $(this).children('.col-sm-10').attr("class", "past col-sm-10 description")
+          } else {
+            $(this).children('.col-sm-10').attr("class", "future col-sm-10 description")
+          }
+      })
+    }
+    currentTime();
+    saveButton.on("click",function(event){
+        event.preventDefault()
+        var text= $(this).siblings(".col-sm-10").val().replace(key)
+        var key = $(this).parent().attr("id")
 
-//  function(event){
-//     event.getElementsByClassName('saveBtn');
-// console.log("click")
-// }
-saveButton.on('click')
-
-
-
+        localStorage.setItem(key, JSON.stringify(text))
+    })
+    $("#9 textarea").val(JSON.parse(localStorage.getItem("9")))
+    $("#10 textarea").val(JSON.parse(localStorage.getItem("10")))
+    $("#11 textarea").val(JSON.parse(localStorage.getItem("11")))
+    $("#12 textarea").val(JSON.parse(localStorage.getItem("12")))
+    $("#13 textarea").val(JSON.parse(localStorage.getItem("13")))
+    $("#14 textarea").val(JSON.parse(localStorage.getItem("14")))
+    $("#15 textarea").val(JSON.parse(localStorage.getItem("15")))
+    $("#16 textarea").val(JSON.parse(localStorage.getItem("16")))
+    $("#17 textarea").val(JSON.parse(localStorage.getItem("17")))
+})
 
 
 
